2ec6b2a7419c321871af1829d03dd418dd794396
SHION0121 <kd1339948@st.kobedenshi.ac.jp>
2024-04-19 09:41:46 +0900

    変数追加
    int型変数m_test

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,8 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+	m_test = 1;
+
 	return true;
 }
 
