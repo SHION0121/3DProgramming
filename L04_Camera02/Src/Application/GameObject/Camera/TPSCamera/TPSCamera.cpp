0db515283fea3a72eb4174660b5af0a49b843d2a
SHION0121 <kd1339948@st.kobedenshi.ac.jp>
2024-07-01 14:37:31 +0900

    カメラ補正

--- L04_Camera02/Src/Application/GameObject/Camera/TPSCamera/TPSCamera.cpp
+++ L04_Camera02/Src/Application/GameObject/Camera/TPSCamera/TPSCamera.cpp
@@ -19,9 +19,7 @@ void TPSCamera::Update()
 {
 	//ターゲットの行列
 	Math::Matrix _targetMat = Math::Matrix::Identity;
-
 	const std::shared_ptr<const KdGameObject>_spTarget = m_wpTarget.lock();
-
 	if (_spTarget)
 	{
 		_targetMat = Math::Matrix::CreateTranslation(_spTarget->GetPos());
@@ -29,10 +27,63 @@ void TPSCamera::Update()
 
 	//カメラの回転
 	UpdateRotateByMouse();
-
 	m_mRotation = GetRotationMatrix();
-
 	m_mWorld = m_mLocalPos * m_mRotation * _targetMat;
+
+	//↓めり込み防止の為の座標補正計算
+	//①当たり判定（レイ判定）用の情報作成
+	KdCollider::RayInfo _rayInfo;
+	//レイの発射位置情報
+	_rayInfo.m_pos = GetPos();
+	//レイの発射方向を設定
+	_rayInfo.m_dir = Math::Vector3::Down;
+	//レイの長さ
+	_rayInfo.m_range = 1000.0f;
+	//レイの当たり判定をしたいタイプ設定
+	_rayInfo.m_type = KdCollider::TypeGround;
+	if (_spTarget)
+	{
+		Math::Vector3 _targetPos = _spTarget->GetPos();
+		_targetPos.y += 0.1f;
+		_rayInfo.m_dir = _targetPos - GetPos();
+		_rayInfo.m_range = _rayInfo.m_dir.Length();
+		_rayInfo.m_dir.Normalize();
+	}
+
+	//②HIT対象オブジェクトに総当たり
+	for (std::weak_ptr<KdGameObject>wpGameObj : m_wpHitObjectList)
+	{
+		std::shared_ptr<KdGameObject>spGameObj = wpGameObj.lock();
+		if (spGameObj)
+		{
+			std::list<KdCollider::CollisionResult> _retRayList;
+			spGameObj->Intersects(_rayInfo, &_retRayList);
+
+			//③結果を使って座標を補完する
+			//レイに当たったリストから一番近いオブジェクトを検出
+			float maxOverLap = 0;
+			Math::Vector3 hitPos = {};
+			bool hit = false;
+			for (auto& ret : _retRayList)
+			{
+				//レイを遮断し、オーバーした長さが一番長いものを探す
+				if (maxOverLap < ret.m_overlapDistance)
+				{
+					maxOverLap = ret.m_overlapDistance;
+					hitPos = ret.m_hitPos;
+					hit = true;
+				}
+			}
+
+			//何かしらの障害物に当たっている
+			if (hit)
+			{
+				Math::Vector3 _hitPos = hitPos;
+				_hitPos += _rayInfo.m_dir * 0.4f;
+				SetPos(_hitPos);
+			}
+		}
+	}
 
 	CameraBase::Update();
 
